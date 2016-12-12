22503d2479164498864b91f6d5b44900b0f9cfb3
samcheck <samcheckster@gmail.com>
2016-12-12 06:36:33 -0600

    improving with basic code

---
+++ ImpRandomBot.py
@@ -0,0 +1,23 @@
+import hlt
+from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
+import random
+
+
+myID, game_map = hlt.get_init()
+hlt.send_init("ImpRandomPythonBot")
+
+def set_move(square):
+    for direction, neighbor in enumerate(game_map.neighbors(square)):
+        if neighbor.owner != myID and neighbor.strength < square.strength:
+            return Move(square, direction)
+            
+    if square.strength < 5 * square.production:
+        return Move(square, STILL)
+    else:
+        return Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL)))
+
+
+while True:
+    game_map.get_frame()
+    moves = [set_move(square) for square in game_map if square.owner == myID]
+    hlt.send_frame(moves)
